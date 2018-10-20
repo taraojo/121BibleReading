28fdfaf04597f69f42d9a0bae2463451857794e4
Nathan Fisher <gnatfish.nf@gmail.com>
2018-10-20 12:01:03 +0100

    Highlight top plan result

--- src/Results.js
+++ src/Results.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import { Card, Skeleton, Icon } from "antd";
+import { Card, Button } from "antd";
 import Header from "./Header";
 import Content from "./Content";
 import styled from "react-emotion";
@@ -31,6 +30,16 @@ const FAKE_RESULT_RESPONSE = {
 const Plan = styled(Card)`
   border-radius: 4px;
   width: 100%;
+  background-color: ${props => (props.index == 0 ? "#007A9E" : "white")};
+  color: ${props => (props.index == 0 ? "white" : "rgba(0, 0, 0, 0.65)")};
+  h2 {
+    color: ${props => (props.index == 0 ? "white" : "rgba(0, 0, 0, 0.65)")};
+  }
+  margin-bottom: 1em;
+`;
+
+const PlanHeader = styled("h2")`
+  font-size: 2.2em;
 `;
 
 class Results extends Component {
@@ -46,10 +55,17 @@ class Results extends Component {
       <div>
         <Header title="Choose a plan" backButton />
         <Content>
-          {this.state.plans.map(plan => (
-            <Plan>
-              <h2>{plan.name}</h2>
+          {this.state.plans.map((plan, index) => (
+            <Plan index={index}>
+              <PlanHeader>{plan.name}</PlanHeader>
               <p>{plan.description}</p>
+              <Button
+                onClick={() => this.props.history.push(`/plan/${plan.id}`)}
+                ghost
+                block
+              >
+                Select plan
+              </Button>
             </Plan>
           ))}
         </Content>
