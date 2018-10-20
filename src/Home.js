07cfcb7bdff4145ac560e8b4303da9fb0afc8d35
Nathan Fisher <gnatfish.nf@gmail.com>
2018-10-20 12:28:50 +0100

    Landing page logo

--- src/Home.js
+++ src/Home.js
@@ -1,32 +1,33 @@
 import React, { Component } from "react";
-import { Button, Skeleton } from "antd";
+import { Button } from "antd";
 import Content from "./Content";
+import Logo from "./img/biblemate_logo.svg";
 
 class Home extends Component {
   render() {
     return (
       <Content>
-        <div style={{ display: "flex", flexDirection: "column" }}>
-          <div style={{ minHeight: "45vh" }} />
-          <div>
-            <Skeleton />
-            <Button
-              style={{}}
-              size="large"
-              onClick={() => {
-                this.props.history.push("/choices");
-              }}
-              type="primary"
-              block
-            >
-              Find me plans
-            </Button>
-            <Button
-              style={{ marginLeft: "auto", display: "flex", marginTop: "10px" }}
-            >
-              Have code?
-            </Button>
+        <div
+          style={{ display: "flex", flexDirection: "column", padding: "18px" }}
+        >
+          <div style={{ padding: "38px 38px 0 38px" }}>
+            <img src={Logo} />
           </div>
+          <p style={{ textAlign: "center", fontSize: "16px" }}>
+            An easy, accessible way to read the bible with other people.
+            <br /> No experience needed.
+          </p>
+          <Button
+            style={{ marginTop: "48px" }}
+            size="large"
+            onClick={() => {
+              this.props.history.push("/choices");
+            }}
+            type="primary"
+            block
+          >
+            Find a plan
+          </Button>
         </div>
       </Content>
     );
