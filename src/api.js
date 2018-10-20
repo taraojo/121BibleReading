49a217ce55c2894f39373d97a0538ddec1ca4743
Joe Ling (laptop) <josephling11@gmail.com>
2018-10-20 12:56:39 +0100

    updated the api.js file

--- src/api.js
+++ src/api.js
@@ -1,19 +1,39 @@
-const API_HOST = "http://localhost/api/v1";
 
-const setContext = () => {};
-
+/**
+ * 
+ * @param {number} id 
+ */
 const getPlan = async id => {
-  const response = await fetch(`${API_HOST}/get_plan/${id}`);
+  const response = await fetch(`${process.API_HOST}/get_plan/${id}`);
   return response.json().payload;
 };
 
+/**
+ * 
+ * @param {number} scenario
+ * 0, 1, 2
+ */
+const getPlans = async scenario =>{
+  const response = await fetch(`${process.API_HOST}/get_plans/${scenario}`);
+  return response.json().payload;
+}
+
+/**
+ * 
+ * @param {number} id 
+ */
 const getStudy = async id => {
-  const response = await fetch(`${API_HOST}/get_study/${id}`);
+  const response = await fetch(`${process.API_HOST}/get_study/${id}`);
   return response.json().payload;
 };
 
+/**
+ * 
+ * @param {number} id 
+ * this is the id of the plan we are wanting to start a plan for
+ */
 const startPlan = async id => {
-  const response = await fetch(`${API_HOST}/start_plan/${id}`, {
+  const response = await fetch(`${process.API_HOST}/start_plan/${id}`, {
     headers: {
       "Content-Type": "application/json"
     },
@@ -23,9 +43,29 @@ const startPlan = async id => {
   return response.json().payload;
 };
 
+/**
+ * 
+ * @param {string} book 
+ * @param {array} verses
+ * verses is an array of strings e.g. "1:1-2", "9:3" 
+ * its POST because its simpler to throw more complex requests
+ * as well as otherwise we would be pretty much using exactly the same 
+ * api bible api the server is using
+ */
+const getBible = async (book, verses) => {
+  const response = await fetch(`${process.API_HOST}/bible/${book}`, {
+    headers: {
+      "Content-Type": "application/json"
+    },
+    method: "POST",
+    body: JSON.stringify(verses)
+  });
+  return response.json().payload;
+};
+
 export default {
-  setContext,
   getPlan,
+  get_plans,
   getStudy,
   startPlan
 };
