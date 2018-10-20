c829fda8b432c5769d76ea92beb3e65f606e349a
Nathan Fisher <gnatfish.nf@gmail.com>
2018-10-20 16:09:20 +0100

    Bible icon link

--- src/Study.js
+++ src/Study.js
@@ -5,12 +5,13 @@ import api from "./api";
 import BlueHeaderImage from "./img/blueBackground.jpeg";
 import { Button } from "antd";
 import { Icon } from "antd";
+import BibleIcon from "./img/bible.svg";
 
 class Study extends Component {
   state = {
     study: {},
     plan: {},
-    showSection: 0,
+    showSection: 0
   };
 
   async componentDidMount() {
@@ -22,22 +23,45 @@ class Study extends Component {
   }
 
   nextSection = () => {
-    this.setState(({showSection}) => ({ showSection: showSection + 1 }));
-  }
+    this.setState(({ showSection }) => ({ showSection: showSection + 1 }));
+  };
 
   previousSection = () => {
-    this.setState(({showSection}) => ({ showSection: showSection - 1 }));
-  }
+    this.setState(({ showSection }) => ({ showSection: showSection - 1 }));
+  };
 
-  getSection = (index) => {
+  getSection = index => {
     const { content } = this.state.study;
     const [entries] = Object.entries(content[index]);
+    const parts = entries[1].split(" ");
+    const book = parts[0];
+    const verses = parts[1];
+
     return (
       <div>
-        <h3 style={{ width: '100%', textTransform: 'capitalize' }}>{entries[0]}</h3>
-        <p style={{ width: '100%' }}>{entries[1]}</p>
-      </div>);
-  }
+        <h3
+          style={{
+            width: "100%",
+            textTransform: "capitalize",
+            display: "inline"
+          }}
+        >
+          {entries[0]}
+        </h3>
+        {entries[0] === "passage" && (
+          <img
+            onClick={() =>
+              this.props.history.push(`/bible?book=${book}&verses=${verses}`)
+            }
+            width={25}
+            src={BibleIcon}
+            style={{ float: "right" }}
+          />
+        )}
+        <p style={{ width: "100%" }}>{entries[1]}</p>
+      </div>
+    );
+  };
 
   render() {
     const { study, plan, showSection } = this.state;
@@ -56,17 +80,15 @@ class Study extends Component {
         <Content>
           {study.content && this.getSection(showSection)}
 
-          {study.content &&
-            <div style={{ textAlign: 'right' }}>
+          {study.content && (
+            <div style={{ textAlign: "right" }}>
               <Button
                 size="large"
                 style={{ margin: "0.5rem", textAlign: "center" }}
                 onClick={this.previousSection}
                 disabled={showSection === 0}
               >
-                <Icon
-                  type="arrow-left"
-                />
+                <Icon type="arrow-left" />
               </Button>
               <Button
                 type="primary"
@@ -75,11 +97,10 @@ class Study extends Component {
                 onClick={this.nextSection}
                 disabled={showSection === study.content.length}
               >
-                <Icon
-                  type="arrow-right"
-                />
+                <Icon type="arrow-right" />
               </Button>
-            </div>}
+            </div>
+          )}
         </Content>
       </div>
     );
